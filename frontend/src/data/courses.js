58dccb0e6133c29906b8e27dd831e0eb8cd2a545
Francois Grundling <francoisgrundling90@gmail.com>
2026-08-15 09:17:58 +0200

    (feature) DEV-2_CreateHomeCoursesView
    - Create CourseCard component
    - Plug into App.jsx
    - Use dummy data for now

---
+++ frontend/src/data/courses.js
@@ -0,0 +1,12 @@
+export const courses = [
+  { id: 1, name: "Royal Johannesburg & Kensington", location: "Johannesburg, Gauteng", holes: 18, par: 72, price: 950, rating: 4.8, image: "from-emerald-500 to-green-700" },
+  { id: 2, name: "Glendower Golf Club", location: "Ekurhuleni, Gauteng", holes: 18, par: 72, price: 880, rating: 4.7, image: "from-teal-500 to-emerald-700" },
+  { id: 3, name: "Gary Player Country Club", location: "Sun City, North West", holes: 18, par: 72, price: 1450, rating: 4.9, image: "from-lime-500 to-green-700" },
+  { id: 4, name: "Leopard Creek", location: "Malelane, Mpumalanga", holes: 18, par: 72, price: 2100, rating: 5.0, image: "from-green-600 to-emerald-800" },
+  { id: 5, name: "Fancourt Montagu", location: "George, Western Cape", holes: 18, par: 72, price: 1250, rating: 4.8, image: "from-cyan-500 to-teal-700" },
+  { id: 6, name: "Durban Country Club", location: "Durban, KwaZulu-Natal", holes: 18, par: 72, price: 1100, rating: 4.7, image: "from-sky-500 to-blue-700" },
+  { id: 7, name: "Pearl Valley", location: "Paarl, Western Cape", holes: 18, par: 72, price: 1350, rating: 4.8, image: "from-emerald-400 to-teal-600" },
+  { id: 8, name: "Erinvale Golf Club", location: "Somerset West, Western Cape", holes: 18, par: 72, price: 900, rating: 4.5, image: "from-green-500 to-lime-700" },
+  { id: 9, name: "Steenberg Golf Club", location: "Cape Town, Western Cape", holes: 18, par: 72, price: 1400, rating: 4.7, image: "from-teal-600 to-cyan-800" },
+  { id: 10, name: "Arabella Golf Club", location: "Kleinmond, Western Cape", holes: 18, par: 72, price: 1050, rating: 4.6, image: "from-emerald-600 to-green-800" },
+];
